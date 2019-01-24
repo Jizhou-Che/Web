ece55224c4eded0af87b78994555fb21271d4219
Jizhou Che <che_mail@icloud.com>
2019-01-25 07:55:09 +0800

    A random integer picker based on JavaScript.

---
+++ Random Number Picker/index.js
@@ -0,0 +1,47 @@
+var counting;
+
+window.onload = function(){
+	resize();
+	document.getElementById("button").onclick = Count;
+};
+
+function Count(){
+	document.getElementById("button").onclick = Stop;
+	var from = parseFloat(document.getElementById("from").value);
+	var to = parseFloat(document.getElementById("to").value);
+	if(from <= to && from % 1 == 0 && to % 1 == 0){
+		document.getElementById("button").innerHTML = "Stop!";
+		document.getElementById("from").disabled = true;
+		document.getElementById("to").disabled = true;
+		counting = setInterval(function(){
+			document.getElementById("Number").innerHTML = Math.floor(Math.random() * (to - from + 1)) + from;
+		}, 0.1);
+	}else{
+		alert("Invalid input!");
+	}
+}
+
+function Stop(){
+	document.getElementById("button").onclick = Count;
+	document.getElementById("button").innerHTML = "Start!";
+	document.getElementById("from").disabled = false;
+	document.getElementById("to").disabled = false;
+	clearInterval(counting);
+}
+
+window.onresize = resize;
+function resize(){
+	if(window.innerWidth >= 1180 && window.innerHeight >= 785){
+		if(window.innerWidth / window.innerHeight >= 1180 / 785){
+			document.body.style.backgroundSize = window.innerWidth + "px " + window.innerWidth / 1180 * 785 + "px";
+		}else{
+			document.body.style.backgroundSize = window.innerHeight / 785 * 1180 + "px " + window.innerHeight + "px";
+		}
+	}else if(window.innerWidth < 1180 && window.innerHeight >= 785){
+		document.body.style.backgroundSize = window.innerHeight / 785 * 1180 + "px " + window.innerHeight + "px";
+	}else if(window.innerWidth >= 1180 && window.innerHeight < 785){
+		document.body.style.backgroundSize = window.innerWidth + "px " + window.innerWidth / 1180 * 785 + "px";
+	}else{
+		document.body.style.backgroundSize = "1180px 785px";
+	}
+}
